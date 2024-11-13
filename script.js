e006386646c08f617b8b84517a73af36c4533fd3
Michael Bolling <bollingmr@gmail.com>
2024-11-13 15:48:10 -0300

    add questions, statements, and facts arrays

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
 const button = document.getElementById("mainButton");
 const message = document.getElementById("message");
 
+const questions = [
+  "Did you know that ",
+  "Were you aware that ",
+  "Did you realize that ",
+];
+
+const statements = [
+  "It might surprise you to learn that ",
+  "You might find it interesting that ",
+  "Here's a fun fact: ",
+];
+
+const facts = [
+  "bananas are berries, but strawberries aren’t",
+  "a day on Venus is longer than a year on Venus",
+  "octopuses have three hearts, and two stop beating when they swim",
+  "honey never spoils",
+  "the human brain generates enough electricity to power a small light bulb",
+  "Cleopatra lived closer in time to the invention of the iPhone than to the construction of the Great Pyramid of Giza",
+  "the Eiffel Tower can grow more than six inches taller during the summer due to heat expansion",
+  "the shortest war in history was between Britain and Zanzibar on August 27, 1896, lasting just 38 minutes",
+  "the oldest known recipe is for beer, dating back to around 5,000 BCE",
+  "Russia is so large that it spans 11 time zones",
+  "the Dead Sea is so salty that people can float effortlessly on its surface",
+  "Canada has more lakes than the rest of the world combined",
+  "a group of flamingos is called a 'flamboyance'",
+  "a shrimp's heart is located in its head",
+  "the average human produces enough saliva in a lifetime to fill two swimming pools",
+  "the average cloud weighs about a million pounds",
+  "the only two countries without official rivers are Saudi Arabia and Vatican City",
+  "goats have rectangular pupils, which give them panoramic vision",
+  "a snail can sleep for up to three years",
+  "humans share about 60% of their DNA with bananas",
+  "your tongue is the only muscle in your body that is attached at just one end",
+  "the guillotine was used as a form of execution in France until 1977",
+  "some metals, like gallium, melt in your hand",
+];
+
 const changeMessage = () => {
   message.textContent = "A new message";
 };
